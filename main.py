da5dc7c442a157ed9dbe1e7d7f2ec1e16c75ba5f
Dante Pereyra <dante@gentecnologico.com.ar>
2021-10-18 21:22:12 -0300

    Refactor str multilinea - Ej2

--- main.py
+++ main.py
@@ -3,44 +3,54 @@
 
 
 def welcome():
-    print("\nProgramación II: Primer Proyecto / Herboza - Pereyra \n\
---------------------------------------------------------------------------------------- \n\
-Bienvenido al desallorro de ejercicios de Expresiones regulares, Recursión, Colecciones \n\
-e Intercambio de datos JSON XML. \n\
-Elija un ejercicio, [m] ver el menu o ingrese [s] para Salir. \n\
----------------------------------------------------------------------------------------\n\
-        ")
+    output = """
+    Programación II: Primer Proyecto / Herboza - Pereyra
+    ---------------------------------------------------------------------------------------
+    Bienvenido al desallorro de ejercicios de Expresiones regulares, Recursión, Colecciones
+    e Intercambio de datos JSON XML.
+    Elija un ejercicio, [m] ver el menu o ingrese [s] para Salir.
+    ---------------------------------------------------------------------------------------
+    """
+    print(output)
 
 def menu():
-    print("------------------------------------ \n\
-    MENU DE EJERCICIOS \n\
------------------------------------- \n\
-    > Expresiones Regulares \n\
-        [1] Matriculas Argentinas \n\
-        [2] Numeros < 1900 \n\
-        [3] EXTRA: Python Regex \n\
-    > Recursión \n\
-        [4] Codificar números \n\
-        [5] Aplanar lista de listas \n\
-        [6] Comparar 2 listas de num \n\
-        [7] División entera \n\
-        [8] EXTRA: Fibonacci \n\
-        [9] EXTRA: Sumar digitos de N \n\
-        [0] EXTRA: Sumar valores de lista \n\
-    > Colecciones \n\
-        [a] map, filter y reduce \n\
-        [b] Pi desde suma de N terminos \n\
-    > Formato de intercambios de datos \n\
-        [c] Buscar Estacion \n\
-        [d] Ver Batería JSON \n\
-        [e] Ver Batería XML \n\
-        [j] Ver JSON \n\
-        [x] Ver XML \n\n\
-    > Navegacion \n\
-        [m] Ver Menu \n\
-        [s] Salir \n\
------------------------------------- \n\n\
-    ")
+    menustr = """
+    -----------------------------------------
+    MENU DE EJERCICIOS
+    -----------------------------------------
+                
+        > Navegacion
+            [m] Ver Menu
+            [s] Salir
+
+        > Expresiones Regulares
+            [1] Matriculas Argentinas
+            [2] Numeros < 1900
+            [3] EXTRA: Python Regex
+
+        > Recursión
+            [4] Codificar números
+            [5] Aplanar lista de listas
+            [6] Comparar 2 listas de num
+            [7] División entera
+            [8] EXTRA: Fibonacci
+            [9] EXTRA: Sumar digitos de N
+            [0] EXTRA: Sumar valores de lista
+
+        > Colecciones
+            [a] map, filter y reduce
+            [b] Pi desde suma de N terminos
+
+        > Formato de intercambios de datos
+            [c] Buscar Estacion
+            [d] Ver Batería JSON
+            [e] Ver Batería XML
+            [j] Ver JSON
+            [x] Ver XML
+
+    -----------------------------------------
+    """
+    print(menustr)
 
 
 def select_option():
@@ -54,6 +64,7 @@ def routing(ej):
         ej1_patentes_argentinas()
     elif option == "2":
         print("[2] Numeros < 1900")
+        ej2_numeros_menor_1900()
     elif option == "3":
         print("[3] EXTRA: Python Regex")
     elif option == "4":
@@ -97,32 +108,55 @@ def elegir():
 
 
 def ej1_patentes_argentinas():
-    print("\n1. Las matrículas de las aeronaves en Argentina tienen el siguiente formato de acuerdo a su tipo (donde abc significa 3 letras mayúsculas y 123 3 dígitos):\n")
-    print("LV abc\t\tUso general")
-    print("LQ abc\t\tGubernamental")
-    print("LV-X 123\tExperimentales")
-    print("LV-S 123\tAeronave deportiva liviana")
-    print("LV-SX 123\tAeronave deportiva liviana experimental\n")
-    print("Matrículas válidas:\n\
-    LV-QWE\n\
-    LQ-ABE\n\
-    LV-X443\n\
-    LV-S586\n\
-    LV-SX334\n")
-    print("Matrículas inválidas:\n\
-    LA-123\n\
-    LX-ABC\n\
-    LV\n\
-    LV-344\n")
-    
-    print("Implemente una expresión regular para validar matrículas argentinas.\n")
-    print("SOLUCION: \\bL[VQ]-[A-Z][A-Z][A-Z]|LV-(X\d\d\d|S\d\d\d|SX\d\d\d)\\b \n")
-    print("\nExplicacion: usamos \\b para definir el principio y fin de la patente, usando como ancla los espacios y puntuacion.")
-    print("Tomamos en cuenta las similaridades, las diferencias como asi todos los casos que quedan excluidos.")
-    print("Desarrollamos el caso de las de uso general y las gubernamentales: L[QV]-[A-Z][A-Z][A-Z]")
-    print("Luego desarrollamos los 3 casos de las experimentales y livianas juntas: LV- más las opciones para cada una, separadas por un OR (|)\n\n")
+    patentes = """
+    1. Las matrículas de las aeronaves en Argentina tienen el siguiente formato de acuerdo a su tipo (donde abc significa 3 letras mayúsculas y 123 3 dígitos):
+        LV abc\t\tUso general
+        LQ abc\t\tGubernamental
+        LV-X 123\tExperimentales
+        LV-S 123\tAeronave deportiva liviana
+        LV-SX 123\tAeronave deportiva liviana experimental
+
+        Matrículas válidas:
+        LV-QWE
+        LQ-ABE
+        LV-X443
+        LV-S586
+        LV-SX334
+        
+        Matrículas inválidas:
+        LA-123
+        LX-ABC
+        LV
+        LV-344
+
+    Implemente una expresión regular para validar matrículas argentinas.
+
+    SOLUCION: \\bL[VQ]-[A-Z][A-Z][A-Z]|LV-(X\d\d\d|S\d\d\d|SX\d\d\d)\\b 
+
+    Explicacion: usamos \\b para definir el principio y fin de la patente, usando como ancla los espacios y puntuacion.
+    Tomamos en cuenta las similaridades, las diferencias como asi todos los casos que quedan excluidos.
+    Desarrollamos el caso de las de uso general y las gubernamentales: L[QV]-[A-Z][A-Z][A-Z]
+    Luego desarrollamos los 3 casos de las experimentales y livianas juntas: LV- más las opciones para cada una, separadas por un OR (|)
+    """
+    print(patentes)
     elegir()
 
+
+
+def ej2_numeros_menor_1900():
+    menores = """
+    2. Diseñe una ER para validar cadenas de números naturales menores a 1900.
+
+        SOLUCION: \\b(1[0-8]|[0-9])?[0-9]?[0-9]\\b
+
+    Explicacion: usamos \\b para definir el principio y fin del numero, usando como ancla los espacios y puntuacion.
+    N tiene 4 cifras / la primera es 1 / la segunda es entre 0 y 8 / 2 numeros cualquiera
+    Combinamos los casos de los numeros de 3 o 4 cifras con un OR (|) entre 1 más cualquier cifra entre 0 y 8 ó una cifra 0-9
+    Como el caso minimo es un numero de 1 cifra, el resto de las cifras las ponemos con ? porque pueden estar o no.
+    """
+
+    print(menores)
+    elegir()
 
 
 # Programa principal
